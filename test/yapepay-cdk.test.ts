e8a58405c1b22153ccc485ed2d466a5d0ebb74f0
Wilfredo Yupanqui <willy-pc@MacBook-Pro-de-Wilfredo.local>
2026-04-25 23:47:49 -0400

    Initial commit

---
+++ test/yapepay-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as YapepayCdk from '../lib/yapepay-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/yapepay-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new YapepayCdk.YapepayCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
